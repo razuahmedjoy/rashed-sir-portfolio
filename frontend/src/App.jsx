dd4721e1d2049e531d2feb0bfb54f759a25270c9
razuahmedjoy <rjjoybzs420@gmail.com>
2025-08-17 20:59:51 +0600

    netlify issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import NavbarComponent from './components/NavbarComponent'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
